b01bf45585b3952eeac9d86fc7e315b4319419b6
Aurelio-Dev <ruotolooaurelioo@gmail.com>
2026-06-22 18:16:00 -0300

    add genre filter | add components

---
+++ src/components/SerieItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <article>
+    <h2>{{ serie.titulo }}</h2>
+    <p>Genero: {{ serie.genero }}</p>
+    <p>Nota: {{ serie.nota }}</p>
+    <p>Status: {{ serie.assistida ? "Assistida" : "Nao assistida" }}</p>
+
+    <button @click="$emit('excluir', serie.id)">
+      Excluir
+    </button>
+  </article>
+</template>
+
+<script setup>
+defineProps({
+  serie: {
+    type: Object,
+    required: true
+  }
+});
+
+defineEmits(["excluir"]);
+</script>
---
+++ src/components/SerieLista.vue
@@ -0,0 +1,25 @@
+<template>
+  <p v-if="series.length === 0">Nenhuma serie encontrada.</p>
+
+  <div v-else>
+    <SerieItem
+      v-for="serie in series"
+      :key="serie.id"
+      :serie="serie"
+      @excluir="$emit('excluir', $event)"
+    />
+  </div>
+</template>
+
+<script setup>
+import SerieItem from "./SerieItem.vue";
+
+defineProps({
+  series: {
+    type: Array,
+    required: true
+  }
+});
+
+defineEmits(["excluir"]);
+</script>
